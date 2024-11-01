1add7662ce3cce46d1108bdcec8af684ae9c3914
YEYEv50 <148872414+YEYEv50@users.noreply.github.com>
2024-11-01 21:48:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,5 +25,5 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-git clone https://github.com/chenmozhijin/luci-app-socat package/luci-app-socat
+#git clone https://github.com/chenmozhijin/luci-app-socat package/luci-app-socat
 #git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
