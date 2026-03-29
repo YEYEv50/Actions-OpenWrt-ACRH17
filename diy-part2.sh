edc2ef1c0320853ef2cf36a9559d7b8ce53b757b
YEYEv50 <148872414+YEYEv50@users.noreply.github.com>
2026-03-30 00:38:24 +0800

    Comment out luci-app-easytier clone command
    Comment out the git clone command for luci-app-easytier.

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,4 +37,4 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-ap
 #git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 #git clone https://github.com/chenmozhijin/luci-app-socat package/luci-app-socat
 git clone https://github.com/bfmen/luci-app-wolplus package/luci-app-wolplus
-git clone https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
+#git clone https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
