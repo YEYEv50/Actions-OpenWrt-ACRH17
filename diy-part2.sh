eb6b12909db2b569903981e3cb378e135cde4830
YEYEv50 <148872414+YEYEv50@users.noreply.github.com>
2026-05-21 01:15:59 +0800

    Fix commented removal of luci-app-wol and update git clone

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01
 #rm -rf feeds/luci/themes/luci-theme-argon
 #rm -rf feeds/luci/themes/luci-theme-argon-mod
 #rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/luci/applications/luci-app-wol
+#rm -rf feeds/luci/applications/luci-app-wol
 rm -rf feeds/luci/applications/luci-app-passwall
 #rm -rf feeds/packages/lang/golang
 
@@ -36,4 +36,4 @@ git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aur
 #git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 git clone https://github.com/chenmozhijin/luci-app-socat package/luci-app-socat
 git clone https://github.com/bfmen/luci-app-wolplus package/luci-app-wolplus
-#git clone https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
+git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
